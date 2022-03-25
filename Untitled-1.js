c4155f552588073ff8b33d98e397ff5cfae6d2a7
Joyce Kim <74065115+jkizzle1@users.noreply.github.com>
2022-03-25 06:12:49 -0400

    js

---
+++ Untitled-1.js
@@ -0,0 +1,11 @@
+
+<header>
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-WTERFMNDWX"></script>
+    <script> 
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments)};
+        gtag('js', new Date());
+        
+        gtag('config', 'G-WTERFMNDWX');
+    </script>
+</header>
